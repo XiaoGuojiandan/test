bfbead4fb48ed00b7ff26f00a66eadf7b30ec4e5
XiaoGuojiandan <1193899475@qq.com>
2024-12-26 16:41:33 +0800

    Lisi secondCommit

--- Zhangsan/src/main/java/com/example/Hello.java
+++ Zhangsan/src/main/java/com/example/Hello.java
@@ -4,5 +4,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello everyone！！！I am ZhangSan. Welcome to my group！！！");
         System.out.println("大家好！！，我是张三，欢迎大家加入我的小组！！！");
+
+        System.out.println("张三你好，我是李四！！！");
     }
 }
