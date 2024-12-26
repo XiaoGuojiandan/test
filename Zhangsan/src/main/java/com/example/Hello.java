b0041ea99796c2c97e99609a67de49a6d85e7b24
XiaoGuojiandan <1193899475@qq.com>
2024-12-26 16:12:11 +0800

    Zhangsan firstCommit

---
+++ Zhangsan/src/main/java/com/example/Hello.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello everyone！！！I am ZhangSan. Welcome to my group！！！");
+        System.out.println("大家好！！，我是张三，欢迎大家加入我的小组！！！");
+    }
+}
